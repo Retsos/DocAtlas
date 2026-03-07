df64cb91834c93d4c00a630d93b4a67922bc22d8
pagiannis <pagiannis04@gmail.com>
2026-03-07 14:19:36 +0200

    Improve styles - padding

--- frontend/chatbotClient/src/App.tsx
+++ frontend/chatbotClient/src/App.tsx
@@ -2,7 +2,11 @@ import "./App.css";
 import ChatBot from "./components/ChatBox";
 
 function App() {
-  return <ChatBot />;
+  return (
+    <div className="p-20 h-screen w-full">
+      <ChatBot />
+    </div>
+  );
 }
 
 export default App;
